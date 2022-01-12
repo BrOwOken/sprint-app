43f03069168196af8da2c6f570edd4f5ef4b80f1
Jan Maruščák <maruscak.jan@ssakhk.cz>
2022-01-12 20:24:44 +0100

    connection between node and vue

--- node/index.js
+++ node/index.js
@@ -1,30 +1,36 @@
 var jira = require('jira.js');
 var express = require('express')
+var cors = require('cors')
 
 const client = new jira.Version2Client({
     host: 'https://kybernaproject.atlassian.net',
     authentication: {
         basic: {
             email: 'maruscak.jan@ssakhk.cz',
-            apiToken: 'nAzuGGpTzn2gVhHHCq250D47',
+            apiToken: 'jnOgSVevMswUG0fPSeMsE1D5',
         },
     },
 });
 
 async function getFirstProjectName() {
-    const projects = await client.projects.getAllProjects();
+    var projects = await client.projects.getAllProjects();
 
-    return projects[0].name
+    return projects
 }
 
 var app = express()
 const port = 3000
 
+app.use(cors())
+
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', async function (req, res) {
+    console.log("before")
     var result = await getFirstProjectName()
-  res.send(result)
+    console.log("AFTER")
+    console.log(result)
+  res.send(result[0].name)
 })
 
 app.listen(port, () => {
